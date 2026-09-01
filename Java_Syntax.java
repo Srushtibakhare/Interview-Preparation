1fa2d4fad249191b2226f48b3e4602cb10952e52
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-09-01 20:37:41 +0530

    Start Java fundamentals

---
+++ Java_Syntax.java
@@ -0,0 +1,34 @@
+public class Java_Syntax {
+    public static void main(String[] args) {
+        //basics 
+        System.out.println("Hello World");
+        System.out.println("Java is Fun!");
+
+        //print number
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(3 + 3);
+
+        // string data types
+        String name = "Srushti";
+        System.out.println(name);
+
+        //int data type
+        int myNum = 15;
+        System.out.println(myNum);
+        //also int
+        int myNumber;
+        myNumber = 20;
+        System.out.println(myNumber);
+
+        //print variable 
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        
+
+    }
+}
