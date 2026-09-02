542a04a8c6f25d2535be9422440cfdd2ee2e7667
Srushti Bakhare <srushtibakhare.off@gmail.com>
2026-09-02 12:02:09 +0530

    Java day 2 revise

--- Java_Syntax.java
+++ Java_Syntax.java
@@ -3,32 +3,80 @@ public class Java_Syntax {
         //basics 
         System.out.println("Hello World");
         System.out.println("Java is Fun!");
-
-        //print number
         System.out.println(3);
         System.out.println(358);
         System.out.println(50000);
         System.out.println(3 + 3);
 
-        // string data types
+        //variables
         String name = "Srushti";
-        System.out.println(name);
+        System.out.println(name); //string
 
-        //int data type
         int myNum = 15;
-        System.out.println(myNum);
-        //also int
+        System.out.println(myNum); //int 
+
         int myNumber;
         myNumber = 20;
-        System.out.println(myNumber);
+        System.out.println(myNumber); //int
 
-        //print variable 
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length()); //string length
+
+        //string methoda
+        String text = "Hello World";
+        System.out.println(text.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(text.toLowerCase());   // Outputs "hello world"
+
+        //index 
+        String line = "Please locate where 'locate' occurs!";
+        System.out.println(line.indexOf("locate")); // Outputs 7
+
+        //string concatenation
         String firstName = "John ";
         String lastName = "Doe";
-        String fullName = firstName + lastName;
-        System.out.println(fullName);
+        int Age = 25;
+        String fullname = firstName + lastName;
+        System.out.println("My name is" + fullname + "and my age is" + Age);
+        //System.out.println(firstName.concat(lastName)); prints "John Doe"
 
-        
+        //datatypes
+        int age = 22; 
+        double cgpa = 7.5;
+        float percentage = 85.5f;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println(age); 
+        System.out.println(cgpa);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(passed);
+
+        //type casting
+        int x = 10;
+        double y = x;
+        System.out.println(y); //widening casting
+
+        double a = 10.5;
+        int b = (int) a;
+        System.out.println(b); //narrowing casting
+
+        //operators
+        int p = 20;
+        int q = 8;
+
+        //java uses + for both addition and concatenation
+        System.out.println(p + q); //addition
+        System.out.println(p - q); //subtraction
+        System.out.println(p * q); //multiplication
+        System.out.println(p / q); //division
+        System.out.println(p % q); //modulus
+
+        System.out.println(p > q); //greater than
+        System.out.println(p < q); //less than
+        System.out.println(p == q); //is equal to
+        System.out.println(p != q); //is not equal to
+
 
     }
 }
